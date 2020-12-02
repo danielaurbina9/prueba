677a8be7dc9a6895aa0da342a7e4dae038e4e4f4
danielaurbina9 <73113135+danielaurbina9@users.noreply.github.com>
2020-12-02 17:40:35 -0300

    Create partners.component.ts

---
+++ partners.component.ts
@@ -0,0 +1,13 @@
+import {component.OnInit } from 'angular/core';
+@component({
+selector: 'app-partners' ,
+templateUrl:'./partners.component.html' ,
+styleUrls:['./partners.component.css']
+})
+export class partnersComponent implements OnInit {
+
+constructor() {}
+
+ngOnInt():void{
+}
+}
