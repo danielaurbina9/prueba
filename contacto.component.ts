b34b014830153437adf979062792e431f71587e9
danielaurbina9 <73113135+danielaurbina9@users.noreply.github.com>
2020-12-02 17:42:20 -0300

    Create contacto.component.ts

---
+++ contacto.component.ts
@@ -0,0 +1,13 @@
+import {component.OnInit } from 'angular/core';
+@component({
+selector: 'app-contacto' ,
+templateUrl:'./contacto.component.html' ,
+styleUrls:['./contacto.component.css']
+})
+export class contactoComponent implements OnInit {
+
+constructor() {}
+
+ngOnInt():void{
+}
+}
