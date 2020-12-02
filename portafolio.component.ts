cc4c61258f864c1382c701b0020d4531abfb83bd
danielaurbina9 <73113135+danielaurbina9@users.noreply.github.com>
2020-12-02 17:41:17 -0300

    Create portafolio.component.ts

---
+++ portafolio.component.ts
@@ -0,0 +1,13 @@
+import {component.OnInit } from 'angular/core';
+@component({
+selector: 'app-portafolio' ,
+templateUrl:'./portafolio.component.html' ,
+styleUrls:['./portafolio.component.css']
+})
+export class portafolioComponent implements OnInit {
+
+constructor() {}
+
+ngOnInt():void{
+}
+}
